bd5a890bc13e4a3043da88fe61b94bd7736a4c18
Md Asaduzzaman <zasad.cse@gmail.com>
2026-07-01 22:37:48 +0600

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> ans;
+        for (const auto& s : strs) {
+            vector<int> count(26, 0);
+            for (char c : s) {
+                count[c - 'a']++; // for ascii values
+            }
+            string key = to_string(count[0]);
+            for (int i = 1; i < 26; ++i) {
+                key += ',' + to_string(count[i]);  // convert frequency counts to string
+            }
+            ans[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (const auto& pair : ans) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
+
+// TC -> O(M * N)
